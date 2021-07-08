eb766effaae00088923e0f028ebd537c6ac5d918
Tuan Pham Minh <tuanpm@foobla.com>
2021-07-08 14:10:18 +0700

    change SLOT_BLOCKS_TARGET, NUMBER_ZERO_BITS_PLOT_FILTER, SUB_EPOCH_BLOCKS, EPOCH_BLOCKS

--- shamrock/consensus/default_constants.py
+++ shamrock/consensus/default_constants.py
@@ -3,7 +3,7 @@ from shamrock.util.ints import uint64
 from .constants import ConsensusConstants
 
 testnet_kwargs = {
-    "SLOT_BLOCKS_TARGET": 32,
+    "SLOT_BLOCKS_TARGET": 64,
     "MIN_BLOCKS_PER_CHALLENGE_BLOCK": 16,  # Must be less than half of SLOT_BLOCKS_TARGET
     "MAX_SUB_SLOT_BLOCKS": 128,  # Must be less than half of SUB_EPOCH_BLOCKS
     "NUM_SPS_SUB_SLOT": 64,  # Must be a power of 2
@@ -11,18 +11,18 @@ testnet_kwargs = {
     # DIFFICULTY_STARTING is the starting difficulty for the first epoch, which is then further
     # multiplied by another factor of DIFFICULTY_CONSTANT_FACTOR, to be used in the VDF iter calculation formula.
     # "DIFFICULTY_CONSTANT_FACTOR": 2 ** 67,
-    "DIFFICULTY_CONSTANT_FACTOR": 2 ** 43,
+    "DIFFICULTY_CONSTANT_FACTOR": 2 ** 57,
     "DIFFICULTY_STARTING": 7,
     # "DIFFICULTY_STARTING": 30,
     "DIFFICULTY_CHANGE_MAX_FACTOR": 3,  # The next difficulty is truncated to range [prev / FACTOR, prev * FACTOR]
     # These 3 constants must be changed at the same time
     # "SUB_EPOCH_BLOCKS": 384,  # The number of blocks per sub-epoch, mainnet 384
-    "SUB_EPOCH_BLOCKS": 384,   # The number of blocks per sub-epoch, mainnet 384
+    "SUB_EPOCH_BLOCKS": 192,   # The number of blocks per sub-epoch, mainnet 384
     # "EPOCH_BLOCKS": 4608,  # The number of blocks per epoch, mainnet 4608. Must be multiple of SUB_EPOCH_SB
-    "EPOCH_BLOCKS": 4608,  # The number of blocks per epoch, mainnet 4608. Must be multiple of SUB_EPOCH_SB
+    "EPOCH_BLOCKS": 2304,  # The number of blocks per epoch, mainnet 4608. Must be multiple of SUB_EPOCH_SB
     "SIGNIFICANT_BITS": 8,  # The number of bits to look at in difficulty and min iters. The rest are zeroed
     "DISCRIMINANT_SIZE_BITS": 1024,  # Max is 1024 (based on ClassGroupElement int size)
-    "NUMBER_ZERO_BITS_PLOT_FILTER": 5,  # H(plot signature of the challenge) must start with these many zeroes
+    "NUMBER_ZERO_BITS_PLOT_FILTER": 8,  # H(plot signature of the challenge) must start with these many zeroes
     "MIN_PLOT_SIZE": 32,  # 32 for mainnet
     "MAX_PLOT_SIZE": 50,
     "SUB_SLOT_TIME_TARGET": 600,  # The target number of seconds per slot, mainnet 600
@@ -33,10 +33,10 @@ testnet_kwargs = {
     # We override this value based on the chain being run (testnet0, testnet1, mainnet, etc)
     # Default used for tests is std_hash(b'')
     # "GENESIS_CHALLENGE": bytes.fromhex("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"),
-    "GENESIS_CHALLENGE": bytes.fromhex("14a71b302e27a52031d6e0bfeba759ea6c50bfa4f360b4616993e005ef54d568"),
+    "GENESIS_CHALLENGE": bytes.fromhex("0cad4b3ea1e76ec15c2fe8470482e37804a2eb6f1ab1586c509a7a1fa8fd1032"),
     # Forks of shamrock should change this value to provide replay attack protection. This is set to mainnet genesis chall
     # "AGG_SIG_ME_ADDITIONAL_DATA": bytes.fromhex("ccd5bb71183532bff220ba46c268991a3ff07eb358e8255a65c30a2dce0e5fbb"),
-    "AGG_SIG_ME_ADDITIONAL_DATA": bytes.fromhex("cd56f7e34367d49b6f867a3b6f1fc5082947493804ac9b685c9d5a5f12b7f118"),
+    "AGG_SIG_ME_ADDITIONAL_DATA": bytes.fromhex("7e846aee6d59856df623f2a16f96f4af65cac3493b89212ff534f9bdc55161a4"),
     "GENESIS_PRE_FARM_POOL_PUZZLE_HASH": bytes.fromhex(
         # "d23da14695a188ae5708dd152263c4db883eb27edeb936178d4d988b8f3ce5fc"
         "6e95df6cad92e52ac0cac6d71be4daef0302ecbcfb7168747d12a53faed69079"
