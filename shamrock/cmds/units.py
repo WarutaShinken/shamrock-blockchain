409c03a16fa5483572ee6590e505df70772f5ad5
Tuan Pham Minh <tuanpm@foobla.com>
2021-07-08 11:07:29 +0700

    rebranding

--- shamrock/cmds/units.py
+++ shamrock/cmds/units.py
@@ -3,7 +3,7 @@ from typing import Dict
 # The rest of the codebase uses mojos everywhere.
 # Only use these units for user facing interfaces.
 units: Dict[str, int] = {
-    "shamrock": 10 ** 12,  # 1 shamrock (XCH) is 1,000,000,000,000 mojo (1 trillion)
+    "shamrock": 10 ** 12,  # 1 shamrock (SRN) is 1,000,000,000,000 mojo (1 trillion)
     "mojo:": 1,
     "colouredcoin": 10 ** 3,  # 1 coloured coin is 1000 colouredcoin mojos
 }
