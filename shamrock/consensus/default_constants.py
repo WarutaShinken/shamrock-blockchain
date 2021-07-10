230c062e646e995b9277e3811cdf26e07525ce0f
Tuan Pham Minh <tuanpm@foobla.com>
2021-07-10 16:46:00 +0700

    change constants

--- shamrock/consensus/default_constants.py
+++ shamrock/consensus/default_constants.py
@@ -5,10 +5,10 @@ from .constants import ConsensusConstants
 testnet_kwargs = {
     "SLOT_BLOCKS_TARGET": 512,
     "MIN_BLOCKS_PER_CHALLENGE_BLOCK": 256,  # Must be less than half of SLOT_BLOCKS_TARGET
-    "MAX_SUB_SLOT_BLOCKS": 1024,  # Must be less than half of SUB_EPOCH_BLOCKS
+    "MAX_SUB_SLOT_BLOCKS": 2048,  # Must be less than half of SUB_EPOCH_BLOCKS
     "NUM_SPS_SUB_SLOT": 64,  # Must be a power of 2
     # "SUB_SLOT_ITERS_STARTING": 2 ** 27,
-    "SUB_SLOT_ITERS_STARTING": 2 ** 17,
+    "SUB_SLOT_ITERS_STARTING": 2 ** 7,
     # DIFFICULTY_STARTING is the starting difficulty for the first epoch, which is then further
     # multiplied by another factor of DIFFICULTY_CONSTANT_FACTOR, to be used in the VDF iter calculation formula.
     # "DIFFICULTY_CONSTANT_FACTOR": 2 ** 67,
@@ -18,9 +18,9 @@ testnet_kwargs = {
     "DIFFICULTY_CHANGE_MAX_FACTOR": 3,  # The next difficulty is truncated to range [prev / FACTOR, prev * FACTOR]
     # These 3 constants must be changed at the same time
     # "SUB_EPOCH_BLOCKS": 384,  # The number of blocks per sub-epoch, mainnet 384
-    "SUB_EPOCH_BLOCKS": 3072,   # The number of blocks per sub-epoch, mainnet 384
+    "SUB_EPOCH_BLOCKS": 4096,   # The number of blocks per sub-epoch, mainnet 384
     # "EPOCH_BLOCKS": 4608,  # The number of blocks per epoch, mainnet 4608. Must be multiple of SUB_EPOCH_SB
-    "EPOCH_BLOCKS": 36864,  # The number of blocks per epoch, mainnet 4608. Must be multiple of SUB_EPOCH_SB
+    "EPOCH_BLOCKS": 49152,  # The number of blocks per epoch, mainnet 4608. Must be multiple of SUB_EPOCH_SB
     "SIGNIFICANT_BITS": 8,  # The number of bits to look at in difficulty and min iters. The rest are zeroed
     "DISCRIMINANT_SIZE_BITS": 1024,  # Max is 1024 (based on ClassGroupElement int size)
     "NUMBER_ZERO_BITS_PLOT_FILTER": 9,  # H(plot signature of the challenge) must start with these many zeroes
