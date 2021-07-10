2a70f232c2512d6c0db18d575bde4945c50da8ea
Tuan Pham Minh <tuanpm@foobla.com>
2021-07-10 15:48:32 +0700

    change ratelimit, contants

--- shamrock/consensus/default_constants.py
+++ shamrock/consensus/default_constants.py
@@ -3,16 +3,16 @@ from shamrock.util.ints import uint64
 from .constants import ConsensusConstants
 
 testnet_kwargs = {
-    "SLOT_BLOCKS_TARGET": 256,
-    "MIN_BLOCKS_PER_CHALLENGE_BLOCK": 128,  # Must be less than half of SLOT_BLOCKS_TARGET
+    "SLOT_BLOCKS_TARGET": 512,
+    "MIN_BLOCKS_PER_CHALLENGE_BLOCK": 256,  # Must be less than half of SLOT_BLOCKS_TARGET
     "MAX_SUB_SLOT_BLOCKS": 1024,  # Must be less than half of SUB_EPOCH_BLOCKS
     "NUM_SPS_SUB_SLOT": 64,  # Must be a power of 2
     # "SUB_SLOT_ITERS_STARTING": 2 ** 27,
-    "SUB_SLOT_ITERS_STARTING": 2 ** 17,
+    "SUB_SLOT_ITERS_STARTING": 2 ** 7,
     # DIFFICULTY_STARTING is the starting difficulty for the first epoch, which is then further
     # multiplied by another factor of DIFFICULTY_CONSTANT_FACTOR, to be used in the VDF iter calculation formula.
     # "DIFFICULTY_CONSTANT_FACTOR": 2 ** 67,
-    "DIFFICULTY_CONSTANT_FACTOR": 2 ** 3,
+    "DIFFICULTY_CONSTANT_FACTOR": 2 ** 1,
     "DIFFICULTY_STARTING": 7,
     # "DIFFICULTY_STARTING": 30,
     "DIFFICULTY_CHANGE_MAX_FACTOR": 3,  # The next difficulty is truncated to range [prev / FACTOR, prev * FACTOR]
@@ -23,7 +23,7 @@ testnet_kwargs = {
     "EPOCH_BLOCKS": 36864,  # The number of blocks per epoch, mainnet 4608. Must be multiple of SUB_EPOCH_SB
     "SIGNIFICANT_BITS": 8,  # The number of bits to look at in difficulty and min iters. The rest are zeroed
     "DISCRIMINANT_SIZE_BITS": 1024,  # Max is 1024 (based on ClassGroupElement int size)
-    "NUMBER_ZERO_BITS_PLOT_FILTER": 8,  # H(plot signature of the challenge) must start with these many zeroes
+    "NUMBER_ZERO_BITS_PLOT_FILTER": 9,  # H(plot signature of the challenge) must start with these many zeroes
     "MIN_PLOT_SIZE": 32,  # 32 for mainnet
     "MAX_PLOT_SIZE": 50,
     "SUB_SLOT_TIME_TARGET": 600,  # The target number of seconds per slot, mainnet 600
