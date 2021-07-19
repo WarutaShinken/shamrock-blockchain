c6abde0f463ec72da523862a058eca32d388c581
Tuan Pham Minh <tuanpm@foobla.com>
2021-07-19 08:43:06 +0700

    change constants

--- shamrock/consensus/default_constants.py
+++ shamrock/consensus/default_constants.py
@@ -7,12 +7,12 @@ testnet_kwargs = {
     "MIN_BLOCKS_PER_CHALLENGE_BLOCK": 16,  # Must be less than half of SLOT_BLOCKS_TARGET
     "MAX_SUB_SLOT_BLOCKS": 128,  # Must be less than half of SUB_EPOCH_BLOCKS
     "NUM_SPS_SUB_SLOT": 64,  # Must be a power of 2
-    "SUB_SLOT_ITERS_STARTING": 2 ** 27,
+    "SUB_SLOT_ITERS_STARTING": 2 ** 10,
     # DIFFICULTY_STARTING is the starting difficulty for the first epoch, which is then further
     # multiplied by another factor of DIFFICULTY_CONSTANT_FACTOR, to be used in the VDF iter calculation formula.
-    "DIFFICULTY_CONSTANT_FACTOR": 2 ** 47,
+    "DIFFICULTY_CONSTANT_FACTOR": 2,
     "DIFFICULTY_STARTING": 7,
-    "DIFFICULTY_CHANGE_MAX_FACTOR": 1,  # The next difficulty is truncated to range [prev / FACTOR, prev * FACTOR]
+    "DIFFICULTY_CHANGE_MAX_FACTOR": 2,  # The next difficulty is truncated to range [prev / FACTOR, prev * FACTOR]
     # These 3 constants must be changed at the same time
     "SUB_EPOCH_BLOCKS": 384,  # The number of blocks per sub-epoch, mainnet 384
     "EPOCH_BLOCKS": 768,  # The number of blocks per epoch, mainnet 4608. Must be multiple of SUB_EPOCH_SB
