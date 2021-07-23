628f7121c1dc58958b05738f5135314926725555
Tuan Pham Minh <tuanpm@foobla.com>
2021-07-23 13:13:53 +0700

    change constants

--- shamrock/rpc/full_node_rpc_api.py
+++ shamrock/rpc/full_node_rpc_api.py
@@ -399,7 +399,7 @@ class FullNodeRpcApi:
 
         delta_iters = newer_block.total_iters - older_block.total_iters
         # ESTIMATE_SPACE_DIVIDED = int(pow(2, 27))
-        ESTIMATE_SPACE_DIVIDED = 1
+        ESTIMATE_SPACE_DIVIDED = 1/(int(pow(2, 10)))
         weight_div_iters = delta_weight / delta_iters / ESTIMATE_SPACE_DIVIDED
         additional_difficulty_constant = self.service.constants.DIFFICULTY_CONSTANT_FACTOR
         eligible_plots_filter_multiplier = 2 ** self.service.constants.NUMBER_ZERO_BITS_PLOT_FILTER
