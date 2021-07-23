825e0a45915e1da42833135c3e04ce866eeb40f1
Tuan Pham Minh <tuanpm@foobla.com>
2021-07-23 13:18:10 +0700

    change constants

--- shamrock/rpc/full_node_rpc_api.py
+++ shamrock/rpc/full_node_rpc_api.py
@@ -400,14 +400,14 @@ class FullNodeRpcApi:
         delta_iters = newer_block.total_iters - older_block.total_iters
         # ESTIMATE_SPACE_DIVIDED = int(pow(2, 27))
         ESTIMATE_SPACE_DIVIDED = 1/(int(pow(2, 10)))
-        weight_div_iters = delta_weight / delta_iters / ESTIMATE_SPACE_DIVIDED
+        weight_div_iters = delta_weight / delta_iters
         additional_difficulty_constant = self.service.constants.DIFFICULTY_CONSTANT_FACTOR
         eligible_plots_filter_multiplier = 2 ** self.service.constants.NUMBER_ZERO_BITS_PLOT_FILTER
         network_space_bytes_estimate = (
             UI_ACTUAL_SPACE_CONSTANT_FACTOR
             * weight_div_iters
             * additional_difficulty_constant
-            * eligible_plots_filter_multiplier
+            * eligible_plots_filter_multiplier / ESTIMATE_SPACE_DIVIDED
         )
         return {"space": uint128(int(network_space_bytes_estimate)),
                 "delta_weight": delta_weight,
