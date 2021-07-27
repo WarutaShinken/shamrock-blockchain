f8b77ff672f44d7587691157c62656b64fd1296d
Tuan Pham Minh <tuanpm@foobla.com>
2021-07-27 09:47:29 +0700

    decrease SUB_EPOCH_BLOCKS, EPOCH_BLOCKS for testing

--- shamrock/consensus/default_constants.py
+++ shamrock/consensus/default_constants.py
@@ -14,8 +14,8 @@ testnet_kwargs = {
     "DIFFICULTY_STARTING": 7,
     "DIFFICULTY_CHANGE_MAX_FACTOR": 2,  # The next difficulty is truncated to range [prev / FACTOR, prev * FACTOR]
     # These 3 constants must be changed at the same time
-    "SUB_EPOCH_BLOCKS": 3600,  # The number of blocks per sub-epoch, mainnet 3600 ~ 2 hours
-    "EPOCH_BLOCKS": 43200,  # The number of blocks per epoch, mainnet 43200 ~ 1 days. Must be multiple of SUB_EPOCH_SB
+    "SUB_EPOCH_BLOCKS": 192,  # The number of blocks per sub-epoch, mainnet 3600 ~ 2 hours
+    "EPOCH_BLOCKS": 384,  # The number of blocks per epoch, mainnet 43200 ~ 1 days. Must be multiple of SUB_EPOCH_SB
     "SIGNIFICANT_BITS": 8,  # The number of bits to look at in difficulty and min iters. The rest are zeroed
     "DISCRIMINANT_SIZE_BITS": 1024,  # Max is 1024 (based on ClassGroupElement int size)
     "NUMBER_ZERO_BITS_PLOT_FILTER": 9,  # H(plot signature of the challenge) must start with these many zeroes
